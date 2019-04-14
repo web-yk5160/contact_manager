f76e85ad4a700e6c9778ccc04672eecde0bb9463
web-yk5160 <rubystudy0123@gmail.com>
2019-04-14 23:39:41 +0900

    Fixed the selected group before/after saving using session

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,10 @@
 class ContactsController < ApplicationController
   before_action :find_contact, only: [:edit, :update, :destroy]
   def index
+    session[:selected_group_id] = params[:group_id]
     if params[:group_id] && !params[:group_id].empty?
       # @contacts = Contact.where(group_id: params[:group_id]).page(params[:page])
-      @contacts = Group.find(params[:group_id]).contacts.page(params[:page])
+      @contacts = Group.find(params[:group_id]).contacts.order(created_at: :desc).page(params[:page])
     else
       @contacts = Contact.order(created_at: :desc).page(params[:page])
     end
@@ -17,7 +18,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Contact was successfully created."
-      redirect_to contacts_path
+      redirect_to contacts_path(previous_query_string)
     else
       render 'new'
     end
@@ -29,7 +30,7 @@ class ContactsController < ApplicationController
   def update
     if @contact.update(contact_params)
       flash[:success] = "Contact was successfully updated."
-      redirect_to contacts_path
+      redirect_to contacts_path(previous_query_string)
     else
       render 'edit'
     end
@@ -38,7 +39,7 @@ class ContactsController < ApplicationController
   def destroy
     @contact.destroy
     flash[:success] = "Contact was successfully deleted."
-    redirect_to contacts_path
+    redirect_to contacts_path(previous_query_string)
   end
 
   private
@@ -49,5 +50,9 @@ class ContactsController < ApplicationController
 
   def find_contact
     @contact = Contact.find(params[:id])
+  end
+
+  def previous_query_string
+    session[:selected_group_id] ? { group_id: session[:selected_group_id] } : {}
   end
 end
