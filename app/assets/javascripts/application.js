3366954e9dbb228365042f52987d7a3de7e5874f
web-yk5160 <rubystudy0123@gmail.com>
2019-04-16 22:51:07 +0900

    Customized the navbar in the application layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
+//= require bootstrap/dropdown
 //= require jasny-bootstrap.min
 //= require rails-ujs
 //= require activestorage
