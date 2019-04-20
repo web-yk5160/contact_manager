43d463287332e9a6ca5ffaf1d0780b9cd93ac457
web-yk5160 <rubystudy0123@gmail.com>
2019-04-20 17:05:28 +0900

    Customized application errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from ActiveRecord::RecordNotFound, with: :show_404
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || dashboard_path
@@ -9,6 +11,17 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     after_sign_in_path_for(resource)
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:danger] = "You are not authorized to perform this action"
+    redirect_to action: :index
+  end
+
+  def show_404
+    render template: "errors/404", status: 404
   end
 
   protected
